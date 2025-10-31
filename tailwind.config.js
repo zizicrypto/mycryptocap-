b0e2ea6b43b5825198482690d5def233e430ea2e
zizicrypto <137682725+zizicrypto@users.noreply.github.com>
2025-11-01 00:16:56 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config.js
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
